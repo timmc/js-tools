8bf2fd29d11ebc292e24ef3b0ee0ffcaecc15722
Tim McCormack <cortex@brainonfire.net>
2012-06-06 12:58:52 -0400

    undefined -> null when no val; .without -> .minus (and + unary); + .plus
    Also:
    * JSDoc instead of top comment block for API
    * Spec for handling variety of QS forms
    * Invariants for internal data structures
    * hasOwnProperty guard on dict key checks

--- qs.js
+++ qs.js
@@ -1,74 +1,67 @@
-// This is based on public domain code written in 2011 by Jan Wolter and
-// distributed at <http://unixpapa.com/js/querystring.html>. Modifications
-// in 2012 by Tim McCormack -- still under public domain.
-//
-// Query String Parser
-//
-//    qs= new QueryString()
-//    qs= new QueryString(string)
-//
-//        Create a query string object based on the given query string. If
-//        no string is given, we use the one from the current page by default.
-//
-//    qs.value(key)
-//
-//        Return a value for the named key.  If the key was not defined,
-//        it will return undefined. If the key was multiply defined it will
-//        return the last value set. If it was defined without a value, it
-//        will return an empty string.
-//
-//   qs.values(key)
-//
-//        Return an array of values for the named key. If the key was not
-//        defined, an empty array will be returned. If the key was multiply
-//        defined, the values will be given in the order they appeared on
-//        in the query string.
-//
-//   qs.keys()
-//
-//        Return an array of unique keys in the query string.  The order will
-//        not necessarily be the same as in the original query, and repeated
-//        keys will only be listed once.
-//
-//    QueryString.decode(string)
-//
-//        This static method is an error tolerant version of the builtin
-//        function decodeURIComponent(), modified to also change pluses into
-//        spaces, so that it is suitable for query string decoding. You
-//        shouldn't usually need to call this yourself as the value(),
-//        values(), and keys() methods already decode everything they return.
-//
-//    New additions: qs.without(key, value) and qs.toString(). See function
-//    documentation.
-//
-// Note: W3C recommends that ; be accepted as an alternative to & for
-// separating query string fields. To support that, simply insert a semicolon
-// immediately after each ampersand in the regular expression in the first
-// function below.
+/**
+ * Query String Parser
+ *
+ * Parse URL querystrings, extract keys and values, manipulate them, and
+ * re-serialize with minimal munging.
+ *
+ * This horrid querystring will be interpreted as follows:
+ *  Input: ??a=b&foo=bar&with+space=also%20space&foo=baz&mult=iple=equals&blank=&missing&&&
+ *  Output:  Key          | Value
+ *           ============================
+ *           '?a'         | 'b'
+ *           'foo'        | 'bar'
+ *           'with space' | 'also space'
+ *           'foo'        | 'baz'
+ *           'mult'       | 'iple=equals'
+ *           'blank'      | ''
+ *           'missing'    | <null>
+ *
+ * Data model:
+ * - Keys are strings, possibly empty. A key may be repeated.
+ * - Values are strings, possibly empty, or null (when there is no '=').
+ * - Order of key-value pairs is preserved.
+ *
+ * QueryString objects are intended to be immutable. The plus and minus
+ * methods return new objects that may share some structure with their
+ * previous incarnations, so meddling from the outside may have surprising
+ * consequences.
+ *
+ * This is based on public domain code written in 2011 by Jan Wolter and
+ * distributed at <http://unixpapa.com/js/querystring.html>. Modifications
+ * in 2012 by Tim McCormack -- still under public domain. Tim McCormack's
+ * version is maintained at <https://github.com/timmc/js-tools/>.
+ *
+ * @author Jan Wolter
+ * @author Tim McCormack
+ */
 
 //== Constructor ==//
 
 /**
  * Construct a QueryString object from current location or provided string.
  * If a string is provided, it should either start with a '?' or be empty.
+ * Safe to call without "new" keyword.
+ *
+ * @constructor
+ * @this {QueryString}
+ * @param {string} qs Optional querystring -- should start with '?' or be empty
  */
 function QueryString(qs) {
+    // If no query string  was passed in use the one from the current page
+    if (arguments.length === 0) { qs = location.search; }
+
     // in case 'new' was omitted
     if (!(this instanceof QueryString)) { return new QueryString(qs); }
 
-    // Values may be either a string (possibly empty) or null.
+    // QS values may be either a string (possibly empty) or null.
 
-    /** Dictionary of decoded keys to lists of decoded values */
+    /* Dictionary of decoded keys to non-empty lists of decoded values */
     this.dict = {};
-    /**
-     * List of key/value pairs in order as map with both encoded and decoded
+    /* List of key/value pairs in order as map with both encoded and decoded
      * strings for each (or possibly undefined in the case of values.)
      * Map keys: key_enc, key_dec, val_enc, val_dec
      */
     this.alist = [];
-
-    // If no query string  was passed in use the one from the current page
-    if (typeof qs === 'undefined') qs = location.search;
 
     // Delete leading question mark, if there is one
     if (qs.charAt(0) == '?') qs = qs.substring(1);
@@ -78,9 +71,11 @@ function QueryString(qs) {
     while (match = re.exec(qs)) {
         var key_enc = match[1];
         var val_enc = match[3];
+        val_enc = val_enc === undefined ? null : val_enc;
+
         var key_dec = decodeURIComponent(key_enc.replace(/\+/g,' '));
-        var val_dec = val_enc === undefined ? undefined
-                                            : QueryString.decode(val_enc);
+        var val_dec = val_enc === null ? null : QueryString.decode(val_enc);
+
         if (this.dict[key_dec]) {
             this.dict[key_dec].push(val_dec);
         } else {
@@ -105,16 +100,33 @@ QueryString.mapDict = function mapDict(d, fkv) {
     return ret;
 };
 
-QueryString.filter = function filter(a, fe) {
+/**
+ * Filter an array using a unary filtering function.
+ * @private
+ * @param {array} arr An input array
+ * @param {function} fe Unary function of [{?} -> {boolean}]
+ * @return {array} A new array with values approved by the filtering function.
+ */
+QueryString.filter = function filter(arr, fe) {
     var ret = [];
-    for (var i=0; i < a.length; i++) {
-        if (fe(a[i])) {
-            ret.push(a[i]);
+    for (var i=0; i < arr.length; i++) {
+        if (fe(arr[i])) {
+            ret.push(arr[i]);
         }
     }
     return ret;
 };
 
+/**
+ * This static method is an error tolerant version of the builtin
+ * function decodeURIComponent(), modified to also change pluses into
+ * spaces, so that it is suitable for query string decoding. You
+ * shouldn't usually need to call this yourself as the value(),
+ * values(), and keys() methods already decode everything they return.
+ * @private
+ * @param {string} s URL-encoded string
+ * @return {string}
+ */
 QueryString.decode = function decode(s) {
     s= s.replace(/\+/g,' ');
     s= s.replace(/%([EF][0-9A-F])%([89AB][0-9A-F])%([89AB][0-9A-F])/g,
@@ -141,22 +153,44 @@ QueryString.decode = function decode(s) {
     return s;
 };
 
+/**
+ * The approximate inverse of QueryString.decode.
+ * @private
+ * @param {string} s any string
+ * @return {string} URL-encoded string
+ */
 QueryString.encode = function encode(s) {
     return encodeURIComponent(s).replace(/%20/g, '+');
 };
 
 //== Instance methods ==//
 
+/**
+ * Return first value for the named key. If the key was not defined,
+ * it will return undefined. If the key was multiply defined it will
+ * return the last value set.
+ * @param {string} key Key to query for.
+ * @return {string|null|undefined} Last value for key, or undefined
+ */
 QueryString.prototype.value = function value(key) {
-    var a= this.dict[key];
-    return a ? a[a.length-1] : undefined;
+    if (!this.dict.hasOwnProperty(key)) return;
+    var vs = this.dict[key];
+    return vs[vs.length-1];
 };
 
+/**
+ * Return an array of values for the named key, possibly empty.
+ * @param {string} key Key to query for.
+ * @return {array} Possibly empty array of values.
+ */
 QueryString.prototype.values = function values(key) {
-    var a= this.dict[key];
-    return a ? a : [];
+    return this.dict.hasOwnProperty(key) ? this.dict[key] : [];
 };
 
+/**
+ * Return an array of *unique* keys in the query string in some order.
+ * @return {array} Possibly empty array of the set of key names.
+ */
 QueryString.prototype.keys = function keys() {
     var a= [];
     for (var key in this.dict) {
@@ -167,22 +201,55 @@ QueryString.prototype.keys = function keys() {
 };
 
 /**
- * Create a new QueryString object without any copies of the k=v pair.
+ * Create a new QueryString object with an additional key/value pair.
+ * @param {string} k Key of pair to add
+ * @param {string|null} v Value of pair to add
+ * @return {QueryString} new QueryString object
  */
-QueryString.prototype.without = function without(k, v) {
-    var ret = new QueryString();
-    ret.dict = QueryString.mapDict(this.dict, function(ok, ovs) {
-        var ret = QueryString.filter(ovs, function(el) { return el !== v; });
-        return ret.length === 0 ? undefined : ret;
+QueryString.prototype.plus = function with(k, v) {
+    var retQS = new QueryString();
+    retQS.dict = QueryString.mapDict(this.dict, function(ok, ovs) {
+        if (ok !== k) return ovs;
+        var retVs = ovs.slice();
+        retVs.push(v);
+        return retVs;
     });
-    ret.alist = QueryString.filter(this.alist, function(pair) {
-        return !(pair.key_dec === k && pair.val_dec === v);
-    });
-    return ret;
+    retQS.alist = this.alist.slice();
+    retQS.alist.push({key_enc:QueryString.encode(k), key_dec:k,
+                      val_enc:QueryString.encode(v), val_dec:v});
+    return retQS;
 };
 
 /**
- * Convert QueryString object back into a URL query-string starting with a '?'.
+ * Create a new QueryString object without any copies of specified param.
+ * If called as unary function without(k), params are removed without
+ * checking the value.
+ * @param {string} k Key to remove
+ * @param {string|null} v
+ * @return {QueryString} new QueryString object
+ */
+QueryString.prototype.minus = function minus(k, v) {
+    var checkVals = arguments.length === 2;
+    var retQS = new QueryString();
+    retQS.dict = QueryString.mapDict(this.dict, function(ok, ovs) {
+        var retVs;
+        if (checkVals) {
+            retVs = QueryString.filter(ovs, function(el) { return el !== v; });
+        } else {
+            retVs = ovs;
+        }
+        return retVs.length === 0 ? undefined : retVs;
+    });
+    retQS.alist = QueryString.filter(this.alist, function(pair) {
+        var matches = pair.key_dec === k && (!checkVals || pair.val_dec === v);
+        return !matches;
+    });
+    return retQS;
+};
+
+/**
+ * Convert QueryString object back into a URL query-string.
+ * @return {string} querystring that starts with a '?' or is empty
  */
 QueryString.prototype.toString = function toString() {
     var al = this.alist;
@@ -192,7 +259,7 @@ QueryString.prototype.toString = function toString() {
     for (var i=0; i<al.length; i++) {
         var pair = al[i];
         ret += "&" + pair.key_enc;
-        if (pair.val_enc !== undefined) {
+        if (pair.val_enc !== null) {
             ret += '=' + pair.val_enc;
         }
     }
